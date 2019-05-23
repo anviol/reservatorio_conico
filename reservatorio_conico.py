d464a3d1b04aec9aedd4729b0c761eb8f7dd56f6
André <andrev-eng@ufmg.br>
2019-05-22 21:48:02 -0300

    cabeçalho

--- reservatorio_conico.py
+++ reservatorio_conico.py
@@ -1,6 +1,19 @@
-### Comeco do codigo helloworld.py #############
-### Programa que escreve na tela "Hello World" #
+################################################################################
+#                                                                              #
+#                        Trabalho Pratico Final de ATR                         #
+#                              Reservatorio Conico                             #
+#                                                                              #
+# Professor: Armando Alves Neto                                                #
+#                                                                              #
+# Nome: Andre Vinicius de Oliveira          Matricula: 2013065935              #
+# Nome: Grabriel                                                               #
+#                                                                              #
+# Versão: 1.00                                                                 #
+# Data: 22/05/2019                                                             #
+#                                                                              #
+#                                                                              #
+################################################################################
 
-print ("Hello World")
+print ( " Hello World " )
 
-### Fim do Programa ############################
+################################ Fim do Programa ###############################
