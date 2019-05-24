047de6212865bf6ea1355e0df9f58b10609cd883
André <andrev-eng@ufmg.br>
2019-05-23 22:05:44 -0300

    Volume do tanque pela altura.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # reservatorio_conico
 Trabalho de automação em tempo real UFMG
+
+Para a biblioteca de gráfico temos que instalar a matplotlib:
+	pip install matplotlib
--- reservatorio_conico.py
+++ reservatorio_conico.py
@@ -11,9 +11,56 @@
 # Versão: 1.00                                                                 #
 # Data: 22/05/2019                                                             #
 #                                                                              #
-#                                                                              #
+#                                 Python 3.7.3                                 #
 ################################################################################
 
-print ( " Hello World " )
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  megasena.py
+# 
+#  Copyright 2017 Tavares <tavares@tavares-Inspiron-5558>
+# 
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+# 
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+# 
+# 
+ 
+import matplotlib.pyplot as plt
+import numpy as np
+import random as rand
+
+Cv = 0.5
+
+tam = input('Digite o range do array: ')
+
+try:
+   tam = int(tam)
+except ValueError:
+   print("O valor digitado não é um número")
+   exit()
+
+h = [0]*tam
+Qout = [0]*tam
+
+for i in range(tam):
+    h[i] = i
+    Qout [i] = Cv * (h[i]**0.5)
+
+plt.plot( h, Qout)
+plt.title("Volume X Altura")
+plt.show()
 
 ################################ Fim do Programa ###############################
