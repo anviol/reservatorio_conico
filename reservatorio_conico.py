8f61095ed15346f6753bff519e83c4ad5e9bf54e
André <andrev-eng@ufmg.br>
2019-05-28 20:12:06 -0300

    teste_pid

--- reservatorio_conico.py
+++ reservatorio_conico.py
@@ -13,36 +13,18 @@
 #                                                                              #
 #                                 Python 3.7.3                                 #
 ################################################################################
-
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-#  megasena.py
-# 
-#  Copyright 2017 Tavares <tavares@tavares-Inspiron-5558>
-# 
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-# 
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-# 
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-# 
-# 
  
 import matplotlib.pyplot as plt
 import numpy as np
 import random as rand
+import simple_pid as pid
 
 Cv = 0.5
+R0 = 100
+R1 = 10
+H = 10
+Pi = 3.14
+alpha = (R1 - R0)/H
 
 tam = input('Digite o range do array: ')
 
@@ -54,13 +36,17 @@ except ValueError:
 
 h = [0]*tam
 Qout = [0]*tam
+Qin = [0]*tam
 
 for i in range(tam):
-    h[i] = i
-    Qout [i] = Cv * (h[i]**0.5)
+   h[i] = i
+   Qout [i] = Cv * (h[i]**0.5)
+
+
 
 plt.plot( h, Qout)
 plt.title("Volume X Altura")
 plt.show()
+
 
 ################################ Fim do Programa ###############################
